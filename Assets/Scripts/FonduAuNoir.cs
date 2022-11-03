4f6eb7c932ac8d475c908a3fb585b174c021a87d
1942878 <e.ddemers3@etu.cvm.qc.ca>
2022-11-03 10:23:46 -0400

    init

---
+++ Assets/Scripts/FonduAuNoir.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FonduAuNoir : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        StartFadeIn();
+    }
+
+    private void StartFadeIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        Color col = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+        sprite.color = col;
+        for (float a = 1.0f; a > 0.0f; a -= 0.05f)
+        {
+            col.a = a;
+            sprite.color = col;
+            yield return new WaitForSeconds(.02f);
+        }
+        col.a = 0.0f;
+        sprite.color = col;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
